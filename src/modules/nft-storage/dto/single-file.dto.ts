dda8cef932e8d5dc8a5a89887913645bfd8a9b82
hoando <hoando.dev@gmail.com>
2023-04-18 23:53:05 +0700

    refactor: update response type

--- src/modules/nft-storage/dto/single-file.dto.ts
+++ src/modules/nft-storage/dto/single-file.dto.ts
@@ -3,5 +3,5 @@ import { ApiProperty } from '@nestjs/swagger';
 // you can add validate using class-validator
 export class SingleFileDto {
   @ApiProperty({ type: 'string', format: 'binary' })
-  blog: string;
+  markdown_file: string;
 }
--- src/modules/nft-storage/nft-storage.service.ts
+++ src/modules/nft-storage/nft-storage.service.ts
@@ -1,12 +1,12 @@
 import { nft_storage_token } from './../../constant/index';
 import { Injectable } from '@nestjs/common';
-import { CarReader, FilesSource, NFTStorage } from 'nft.storage';
+import { CIDString, CarReader, FilesSource, NFTStorage } from 'nft.storage';
 
 @Injectable()
 export class NftStorageService {
   constructor() {}
 
-  async storeMardownFile(file: any): Promise<any> {
+  async storeMardownFile(file: any): Promise<CIDString> {
     const storage = new NFTStorage({ token: nft_storage_token });
     return await storage.storeBlob(file);
   }
