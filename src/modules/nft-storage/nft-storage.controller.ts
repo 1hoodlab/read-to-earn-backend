a5cfaad6cb58f6d7e9c74fbd741d5ea8b2026424
hoando <hoando.dev@gmail.com>
2023-04-20 01:09:08 +0700

    refactor: update get content from nft storage

--- src/modules/nft-storage/nft-storage.controller.ts
+++ src/modules/nft-storage/nft-storage.controller.ts
@@ -1,14 +1,14 @@
-import { Body, Controller, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { markdownFilter } from './util';
-import { SingleFileDto } from './dto/single-file.dto';
+import { GetContentDto, SingleFileDto } from './dto/index.dto';
 import { FastifyFileInterceptor } from 'src/interceptor/fastify-file-interceptor';
 import { NftStorageService } from './nft-storage.service';
 import { UPLOAD_FIELD } from './constants';
 import { CIDString } from 'nft.storage';
 
 @Controller('nft-storage')
-@ApiTags('Upload')
+@ApiTags('Nft.Storage')
 export class NftStorageController {
   constructor(private readonly nftStorageService: NftStorageService) {}
   @ApiConsumes('multipart/form-data')
@@ -18,4 +18,7 @@ export class NftStorageController {
     const someData = new Blob([file.buffer]);
     return await this.nftStorageService.storeMardownFile(someData);
   }
+
+  @Get('content')
+  async getContent(@Body() cid: GetContentDto) {}
 }
